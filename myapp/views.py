c653302aeb418c50dcd703dbf11dcda85a5f45e7
Yewo Mhango <mhangoyewoh@gmail.com>
2022-03-02 14:33:51 +0200

    Added a details page

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from myapp.models import Flower
+
 
 def index(request):
     flowers = Flower.objects.all()
 
     return render(request, "myapp/index.html", {'flowers': flowers})
+
+
+def detail(request, slug=None):
+    flower = get_object_or_404(Flower, slug=slug)
+
+    return render(request, 'myapp/detail.html', {'flower': flower})
