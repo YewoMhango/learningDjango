f07d65abb2a777426b6fd7cbcdda350e86107afd
Yewo Mhango <mhangoyewoh@gmail.com>
2022-03-02 19:17:57 +0200

    Added flower categories

--- myapp/models.py
+++ myapp/models.py
@@ -3,17 +3,25 @@ from django.db import models
 from django.urls import reverse
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=255, default='')
+
+    def __str__(self) -> str:
+        return self.title
+
+
 class Flower(models.Model):
     title = models.CharField(max_length=255, default='')
     description = models.TextField(default='')
     slug = models.SlugField(blank=True, default='')
+    category = models.ForeignKey(Category, null=True, on_delete=models.PROTECT)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
 
     def save(self, *args, **kwargs) -> None:
         self.slug = slugify(self.title)
         super(Flower, self).save()
 
-    def get_absolute_url(self):
+    def get_absolute_url(self) -> str:
         return reverse('detail', args=[str(self.slug)])
