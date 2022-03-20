e414e65468b7e3892b02bfdd1ba3bfef4a802e73
Yewo Mhango <mhangoyewoh@gmail.com>
2022-03-20 18:09:30 +0200

    Added search, edit link on title & type anotations

--- myapp/views.py
+++ myapp/views.py
@@ -1,21 +1,29 @@
+from django.http import HttpRequest
 from django.shortcuts import get_object_or_404, render
 
 from myapp.models import Flower
 
 
-def index(request):
-    flowers = Flower.objects.all()
+def index(request: HttpRequest):
+    q = request.GET.get('q', None)
 
-    return render(request, "myapp/index.html", {'flowers': flowers, 'page_title': 'Flowers'})
+    if q is None or q is "":
+        flowers = Flower.objects.all()
+        page_title = "Flowers"
+    elif q is not None:
+        flowers = Flower.objects.filter(title__contains=q)
+        page_title = '“' + q + '” - search results'
+
+    return render(request, "myapp/index.html", {'flowers': flowers, 'flower_count': flowers.count(), 'page_title': page_title})
 
 
-def detail(request, slug=None):
+def detail(request: HttpRequest, slug=None):
     flower = get_object_or_404(Flower, slug=slug)
 
     return render(request, 'myapp/detail.html', {'flower': flower, 'page_title': flower.title})
 
 
-def tags(request, slug=None):
+def tags(request: HttpRequest, slug=None):
     flowers = Flower.objects.filter(tags__slug=slug)
 
     return render(request, 'myapp/index.html', {'flowers': flowers, 'page_title': 'Flowers tagged with "' + slug + '"'})
